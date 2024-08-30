471d084190ee7b6d70ec6df0d1de14ad0832db8a
andrewhsugithub <andrew1mail@gmail.com>
2024-08-31 00:33:36 +0800

    fix: update scripts

--- scripts/start_services.sh
+++ scripts/start_services.sh
@@ -15,15 +15,14 @@ ssh -tt $USER@$MAIN_SERVER << EOF
 
     for service in "\${services[@]}"; do
         echo "Starting \$service service"
-        echo "Running: bash ${CWD}/scripts/run_\$service.sh ${PACKAGES_PATH}/\$service python main.py &"
-        bash "${CWD}/scripts/run_\$service.sh" "${PACKAGES_PATH}/\$service" "python main.py" &
-        pids+=($!)
+        if [ "\$service" == "GPT-SoVITS-Inference" ]; then
+            # Run a different command for GPT-SoVITS-Inference service
+            echo "Running: bash ${CWD}/scripts/special_run.sh ${PACKAGES_PATH}/\$service python pure_api.py &"
+            bash "${CWD}/scripts/special_run.sh" "${PACKAGES_PATH}/\$service" "python pure_api.py" &
+        else
+            echo "Running: bash ${CWD}/scripts/run_\$service.sh ${PACKAGES_PATH}/\$service python main.py &"
+            bash "${CWD}/scripts/run_\$service.sh" "${PACKAGES_PATH}/\$service" "python main.py" &
     done
 
     wait
-
-    # Save PIDs to a file for later use
-    echo "${pids[@]}" > service_pids.txt
-
-    # Exit the SSH session
 EOF
