3a5b32a4d4eff89a74e4eb9cc8c5a0e361c5e2f7
andrewhsugithub <andrew1mail@gmail.com>
2024-08-31 00:40:46 +0800

    fix: update scripts

--- scripts/start_services.sh
+++ scripts/start_services.sh
@@ -17,8 +17,8 @@ ssh -tt $USER@$MAIN_SERVER << EOF
         echo "Starting \$service service"
         if [ "\$service" == "GPT-SoVITS-Inference" ]; then
             # Run a different command for GPT-SoVITS-Inference service
-            echo "Running: bash ${CWD}/scripts/special_run.sh ${PACKAGES_PATH}/\$service python pure_api.py &"
-            bash "${CWD}/scripts/special_run.sh" "${PACKAGES_PATH}/\$service" "python pure_api.py" &
+            echo "Running: bash ${CWD}/scripts/run_\$service.sh ${PACKAGES_PATH}/\$service python pure_api.py &"
+            bash "${CWD}/scripts/run_\$service.sh" "${PACKAGES_PATH}/\$service" "python pure_api.py" &
         else
             echo "Running: bash ${CWD}/scripts/run_\$service.sh ${PACKAGES_PATH}/\$service python main.py &"
             bash "${CWD}/scripts/run_\$service.sh" "${PACKAGES_PATH}/\$service" "python main.py" &
