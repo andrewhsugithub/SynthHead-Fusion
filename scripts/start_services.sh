5b7a2ba509016a27fc43829d22e9af4f1f813cdd
Jason0411202 <113334813+Jason0411202@users.noreply.github.com>
2024-10-19 00:18:09 +0800

    Jason0411202 video cutting (#11)
    * feat: organize to monorepo
    
    * feat: add GPT-Audio
    
    * feat: update README
    
    * fix: update scripts
    
    * fix: update MuseTalk, Real3DPortrait submodule to latest
    
    * fix: update README.md
    
    * fix: update sync_repo script
    
    * fix: update sync_repo script
    
    * fix: update sync_repo script
    
    * fix: update scripts
    
    * fix: update scripts
    
    * fix: update scripts
    
    * fix: update scripts
    
    * feat: update GPT-Audio
    
    * fix: update scripts
    
    * add submodule videoCutting
    
    * add gitignore
    
    ---------
    
    Co-authored-by: andrewhsugithub <andrew1mail@gmail.com>
    Co-authored-by: 鄭皓中 <h0920185003@gmail.com>

---
+++ scripts/start_services.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Usage: to start up all services
+
+# Initialize/Load variables
+source "./scripts/.env.server.sh"
+
+# SSH into the server and execute commands
+ssh -tt $USER@$MAIN_SERVER << EOF
+
+    # Navigate to the packages directory
+    cd $PACKAGES_PATH
+
+    services=("GPT-SoVITS-Inference" "Real3DPortrait" "LivePortrait" "MuseTalk" "GPT-Audio")
+
+    for service in "\${services[@]}"; do
+        echo "Starting \$service service"
+        if [ "\$service" == "GPT-SoVITS-Inference" ]; then
+            # Run a different command for GPT-SoVITS-Inference service
+            echo "Running: bash ${CWD}/scripts/run_\$service.sh ${PACKAGES_PATH}/\$service python pure_api.py &"
+            bash "${CWD}/scripts/run_\$service.sh" "${PACKAGES_PATH}/\$service" "python pure_api.py" &
+        else
+            echo "Running: bash ${CWD}/scripts/run_\$service.sh ${PACKAGES_PATH}/\$service python main.py &"
+            bash "${CWD}/scripts/run_\$service.sh" "${PACKAGES_PATH}/\$service" "python main.py" &
+        fi
+    done
+
+    wait
+EOF
